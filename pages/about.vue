f63b7a54e93e7f2244af3ef148a900e0fb8b3330
Mads Lee Jensen <madsleejensen@gmail.com>
2018-05-09 15:53:37 +0200

    simple vuex integration

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>About</h1>
+  <div>
+    <h1>About {{ $store.state.counter }} {{ hello }}</h1>
+    <a @click="doIncrement">add</a>
+  </div>
 </template>
+
+<script>
+
+export default {
+  name: 'about-page',
+  asyncData(context) {
+    return {
+      count: 123,
+    }
+  },
+  computed: {
+    hello() {
+      console.log(this.$store);
+      return 33;
+    }
+  },
+  methods: {
+    doIncrement() {
+      this.$store.commit('increment');
+    }
+  }
+}
+
+</script>
